0ee076e1693e71c15fcb2e68149c80d8e8a15669
kggu <miikkakortelainen1@gmail.com>
2020-06-15 10:59:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const db = require("./queries");
-const port = 3300;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(
