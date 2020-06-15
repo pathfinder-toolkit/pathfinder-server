34925d40fb53a62b11664ea494a329968c82c2f9
kggu <miikkakortelainen1@gmail.com>
2020-06-15 10:56:35 +0300

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,25 +1,23 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }
-})
 
+connectionString = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+};
+
+const Pool = require("pg").Pool;
+const pool = new Pool(connectionString);
 
 const getBuildings = (request, response) => {
-    pool.query('SELECT * FROM public.buildings;', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
+  pool.query("SELECT * FROM public.buildings;", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
-  module.exports = {
-    getBuildings,
-  }
+module.exports = {
+  getBuildings,
+};
