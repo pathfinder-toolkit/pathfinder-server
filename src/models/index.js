5cf371afc7d431cf119b0744dcff3797178bdac5
Sami Jouppila <sami.jouppila@gmail.com>
2020-06-23 14:34:36 +0300

    require fix

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,6 @@ const { Sequelize } = require("sequelize")
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
    host: process.env.DATABASE_HOST,
    port: 5432,
