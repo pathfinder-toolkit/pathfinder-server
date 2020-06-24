3aef86f0e690ea5cabcb90c62022de81ddf96f45
Sami Jouppila <sami.jouppila@gmail.com>
2020-06-24 09:05:17 +0300

    Cleanup

--- src/controllers/areaController.js
+++ src/controllers/areaController.js
@@ -5,18 +5,19 @@ const Area = db.Area;
 
 const getAreas = async (request, response) => {
     try {
-      const areaObjects = await Area.findAll({
-        attributes:  ['areaName']
-      });
+        const areaObjects = await Area.findAll({
+            attributes:  ['areaName']
+        });
   
-      let areas = [];
-      areaObjects.forEach((areaObject) => {
-        areas.push(areaObject.dataValues.areaName)
-      });
-  
-      response.status(200).json(areas);
+        let areas = [];
+        areaObjects.forEach((areaObject) => {
+            areas.push(areaObject.dataValues.areaName)
+        });
+        
+        response.status(200).json(areas);
+
     } catch (error) {
-      console.error('Unable to connect to the database:', error);
+        console.error('Unable to connect to the database:', error);
     }
 };
 
