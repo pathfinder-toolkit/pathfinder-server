36653ae5e04661e8a2ca009f52f55e29193600b9
Sami Jouppila <sami.jouppila@gmail.com>
2020-06-25 14:35:19 +0300

    BuildingType fix, get /building/:buildingid request returns api v1.5 format building object

--- src/routes/building.js
+++ src/routes/building.js
@@ -75,6 +75,11 @@ buildingRouter.get("/building", (request, response) => {
           "value": "",
           "hasSuggestions": false
         },
+        "description": {
+          "propertyName": "Building type",
+          "value": "",
+          "hasSuggestions": false
+        },
         "image": {
           "propertyName": "Image",
           "value": "",
@@ -276,85 +281,80 @@ buildingRouter.get("/building/:buildingId", (request, response) => {
         "details": {
           "name": {
             "propertyName": "Name",
-            "value": "Talon nimi"
+            "value": "Talon nimi",
+            "hasSuggestions": false
           },
           "area": {
             "propertyName": "Area",
-            "value": "Northern Finland"
+            "value": "Northern Finland",
+            "hasSuggestions": false
           },
           "year": {
             "propertyName": "Construction year",
-            "value": 1900
-          },
-          "floorArea": {
-            "propertyName": "Floor area(in square meters)",
-            "value": 62
-          },
-          "heatedFloorArea": {
-            "propertyName": "Heated floor area(in square meters)",
-            "value": 50
-          },
-          "floorsAmount": {
-            "propertyName": "Amount of floors",
-            "value": 2
+            "value": 1900,
+            "hasSuggestions": false
           },
           "description": {
             "propertyName": "Description of building",
-            "value": "Quisque vulputate enim ligula, sed lobortis metus commodo efficitur. Suspendisse ante lectus, sagittis eu diam a, convallis aliquam eros. Vivamus consequat sagittis nunc in euismod. Vivamus laoreet erat elit. Praesent erat diam, dapibus a purus ac, scelerisque consequat tortor. Aliquam nunc metus, ultricies et lacus a, rutrum feugiat ligula. Proin a enim tortor."
+            "value": "Quisque vulputate enim ligula, sed lobortis metus commodo efficitur. Suspendisse ante lectus, sagittis eu diam a, convallis aliquam eros. Vivamus consequat sagittis nunc in euismod. Vivamus laoreet erat elit. Praesent erat diam, dapibus a purus ac, scelerisque consequat tortor. Aliquam nunc metus, ultricies et lacus a, rutrum feugiat ligula. Proin a enim tortor.",
+            "hasSuggestions": false
+          },
+          "buildingType": {
+            "propertyName": "Building type",
+            "value": "Building type 2",
+            "hasSuggestions": false
           },
           "image": {
             "propertyName": "Image",
-            "value": "/static/media/frontpage_house.294aaf25.jpg"
-          }
-        },
-        "heating": {
-          "heatingSystem": {
-            "propertyName": "Heating System",
-            "value": "Oil",
-            "suggestions": [
-              {
-                "suggestionText": "Nulla urna lorem, porttitor vehicula risus vitae, ultrices commodo nisl. Nullam viverra mollis tortor at vestibulum.",
-                "priority": 95
-              }
-            ],
-            "comments": [
-              {
-                "commentText": "Donec dapibus facilisis nisl vel posuere. Morbi bibendum magna ac lacus vestibulum, eu egestas lacus viverra.",
-                "commentSubject": "Heating System",
-                "commentSecondarySubject": "Oil",
-                "date": "2020-06-15 12:14:34",
-                "author": "John Doe",
-                "sentiment": "positive"
-              }
-            ]
+            "value": "/static/media/frontpage_house.294aaf25.jpg",
+            "hasSuggestions": false
           },
-          "heatingSource": {
-            "propertyName": "Heating Source",
-            "value": "Source 1"
+          "floorsAmount": {
+            "propertyName": "Number of floors",
+            "value": 2,
+            "hasSuggestions": false
+          },
+          "floorArea": {
+            "propertyName": "Floor area(in square meters)",
+            "value": 62,
+            "hasSuggestions": false
+          },
+          "heatedFloorArea": {
+            "propertyName": "Heated floor area(in square meters)",
+            "value": 50,
+            "hasSuggestions": false
+          },
+          "annualConsumption": {
+            "propertyName": "Annual consumption",
+            "value": 800,
+            "hasSuggestions": false
           },
           "annualCost": {
             "propertyName": "Annual cost",
-            "value": 300
-          }
-        },
-        "electricity": {
-          "annualUse": {
-            "propertyName": "Annual use",
-            "value": 500
+            "value": 400,
+            "hasSuggestions": false
           },
-          "annualCost": {
-            "propertyName": "Annual cost",
-            "value": 250
+          "annualHeatingConsumption": {
+            "propertyName": "Annual heating consumption",
+            "value": 300,
+            "hasSuggestions": false
+          },
+          "annualHeatingCost": {
+            "propertyName": "Annual heating cost",
+            "value": 150,
+            "hasSuggestions": false
           }
         },
         "structure": {
           "wallMaterial": {
             "propertyName": "Wall material",
             "value": "Wood",
+            "hasSuggestions": true,
             "suggestions": [
               {
                 "suggestionText": "Sed sapien turpis, rutrum et semper in, eleifend nec elit. Etiam lobortis, ante quis varius vehicula, magna urna ultricies justo, non interdum est lectus a est.",
-                "priority": 30
+                "priority": 30,
+                "suggestionSubject": "Wood",
               }
             ],
             "comments": [
@@ -368,49 +368,177 @@ buildingRouter.get("/building/:buildingId", (request, response) => {
           },
           "wallThickness": {
             "propertyName": "Wall Thickness",
-            "value": 16
+            "value": 16,
+            "hasSuggestions": true
+          },
+          "windowType": {
+            "propertyName": "Window type",
+            "value": "Window 1",
+            "hasSuggestions": true
           },
           "windowAmount": {
-            "propertyName": "Amount of windows",
-            "value": 12
+            "propertyName": "Number of windows",
+            "value": 12,
+            "hasSuggestions": true
+          },
+          "heatedWindowType": {
+            "propertyName": "Heated area window type",
+            "value": "Window 2",
+            "hasSuggestions": true
+          },
+          "heatedWindowAmount": {
+            "propertyName": "Number of windows in heated area",
+            "value": 8,
+            "hasSuggestions": true
           },
           "doorMaterial": {
             "propertyName": "Door material",
-            "value": "Wood"
+            "value": "Wood",
+            "hasSuggestions": true
           },
           "doorAmount": {
             "propertyName": "Amount of doors",
-            "value": 4
+            "value": 4,
+            "hasSuggestions": true
           },
           "roofMaterial": {
             "propertyName": "Roof material",
-            "value": "Roof material 1"
+            "value": "Roof material 1",
+            "hasSuggestions": true
           },
           "roofInsulation": {
             "propertyName": "Roof insulation",
-            "value": true
+            "value": true,
+            "hasSuggestions": true
+          },
+          "roofInsulationThickness": {
+            "propertyName": "Roof insulation thickness",
+            "value": 3.5,
+            "hasSuggestions": true
           }
         },
-        "ventilation": {
-          "ventilationSystem": {
-            "propertyName": "Ventilation system",
-            "value": "Ventilation system 1",
-            "suggestions": [
-              {
-                "suggestionText": "Vivamus laoreet erat elit. Praesent erat diam, dapibus a purus ac, scelerisque consequat tortor. Aliquam nunc metus, ultricies et lacus a, rutrum feugiat ligula.",
-                "priority": 1
-              }
-            ],
-            "comments": [
-              {
-                "commentText": "Proin a enim tortor. Cras vestibulum bibendum libero, a pulvinar turpis eleifend fringilla. Suspendisse et nunc hendrerit, lacinia enim eu, tincidunt dolor.",
-                "commentSubject": "Ventilation system",
-                "date": "2020-06-16 11:44:23",
-                "author": "Jane Doe",
-                "sentiment": "neutral"
-              }
-            ]
+        "heating": {
+          "heatingSystem": [
+            {
+              "propertyName": "Heating System",
+              "value": "Oil",
+              "hasSuggestions": true,
+              "isCurrent": true,
+              "suggestions": [
+                {
+                  "suggestionText": "Nulla urna lorem, porttitor vehicula risus vitae, ultrices commodo nisl. Nullam viverra mollis tortor at vestibulum.",
+                  "priority": 95
+                }
+              ],
+              "comments": [
+                {
+                  "commentText": "Donec dapibus facilisis nisl vel posuere. Morbi bibendum magna ac lacus vestibulum, eu egestas lacus viverra.",
+                  "commentSubject": "Heating System",
+                  "commentSecondarySubject": "Oil",
+                  "date": "2020-06-15 12:14:34",
+                  "author": "John Doe",
+                  "sentiment": "positive"
+                }
+              ]
+            }
+          ],
+          "heatingSource": [
+            {
+              "propertyName": "Heating Source",
+              "value": "Source 1",
+              "hasSuggestions": true,
+              "isCurrent": true
+            }
+          ]
+        },
+        "electric": {
+          "annualUse": {
+            "propertyName": "Annual use",
+            "value": 500,
+            "hasSuggestions": true
+          },
+          "annualCost": {
+            "propertyName": "Annual cost",
+            "value": 250,
+            "hasSuggestions": true
           }
+        },
+        "water": {
+          "heatedWaterEnergySource": [
+            {
+              "propertyName": "Heated water energy source",
+              "value": "Heat energy source 1",
+              "hasSuggestions": true,
+              "isCurrent": true
+            }
+          ]
+        },
+        "ventilation": {
+          "ventilationSystem": [
+            {
+              "propertyName": "Ventilation system",
+              "value": "Ventilation system 1",
+              "hasSuggestions": true,
+              "isCurrent": true,
+              "suggestions": [
+                {
+                  "suggestionText": "Vivamus laoreet erat elit. Praesent erat diam, dapibus a purus ac, scelerisque consequat tortor. Aliquam nunc metus, ultricies et lacus a, rutrum feugiat ligula.",
+                  "priority": 1
+                }
+              ],
+              "comments": [
+                {
+                  "commentText": "Proin a enim tortor. Cras vestibulum bibendum libero, a pulvinar turpis eleifend fringilla. Suspendisse et nunc hendrerit, lacinia enim eu, tincidunt dolor.",
+                  "commentSubject": "Ventilation system",
+                  "date": "2020-06-16 11:44:23",
+                  "author": "Jane Doe",
+                  "sentiment": "neutral"
+                }
+              ]
+            }
+          ]
+        },
+        "renewable": {
+          "heatPump": [
+            {
+              "propertyName": "Heat pump",
+              "value": "Heat pump type 1",
+              "hasSuggestions": true,
+              "isCurrent": true
+            }
+          ],
+          "solarHeat": [
+            {
+              "propertyName": "Solar energy heat",
+              "value": "Vacuum pipes",
+              "hasSuggestions": true,
+              "isCurrent": true
+            }
+          ],
+          "solarElectric": [
+            {
+              "propertyName": "Solar energy electricity",
+              "value": "1-phase",
+              "hasSuggestions": true,
+              "isCurrent": true
+            }
+          ],
+          "bioMass": [
+            {
+              "propertyName": "Biomass energy",
+              "value": "Wooden pellets",
+              "hasSuggestions": true,
+              "isCurrent": true
+            }
+          ],
+          "chp": [
+            {
+              "propertyName": "Combined heat and power",
+              "value": "Combined source 1",
+              "hasSuggestions": true,
+              "isCurrent": true
+            }
+          ]
         }
       }
     );
