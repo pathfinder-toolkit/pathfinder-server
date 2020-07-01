64805c22fbb519c16d287b542b9560d899c050a9
Sami Jouppila <sami.jouppila@gmail.com>
2020-07-01 12:20:43 +0300

    TestBuilding is fetched from the backend, values not working currently

--- src/controllers/buildingController.js
+++ src/controllers/buildingController.js
@@ -13,6 +13,68 @@ const getSampleBuilding = async (request, response) => {
     //await postTestBuilding();
 
     console.log("Test")
+
+    try {
+        const buildingInDatabase = await Building.findOne({
+            attributes:  [['buildingAuthorSub','author'],'slug'],
+            where: {
+                buildingAuthorSub: 'noauthor'
+            },
+            include: {
+                model: Category,
+                as: 'categories',
+                attributes: ['categoryName'],
+                include: {
+                    model: Component,
+                    through: {
+                        attributes: []
+                    },
+                    as: 'components',
+                    attributes: ['isCurrent','usageStartYear'],
+                    include: [{
+                        model: ComponentMeta,
+                        as: 'meta',
+                        attributes: ['componentDescription','componentName','hasSuggestions','subject']
+                    },{
+                        model: ComponentValue,
+                        as: 'value',
+                        attributes: ['valueString', 'valueDate', 'valueInt', 'valueDouble', 'valueText']
+                    }]
+                }
+            }
+        });
+
+        const buildingJSON = buildingInDatabase.toJSON();
+
+        const buildingObject = {};
+        buildingObject.author = buildingJSON.author;
+        buildingObject.slug = buildingJSON.slug;
+
+        buildingJSON['categories'].map((category, index) => {
+            const currentCategory = {};
+            
+            category.components.map((component, index) => {
+                console.log(component);
+                const currentComponent = {};
+                currentComponent.componentDescription = component.meta.componentDescription;
+                let value;
+                currentComponent.hasSuggestions = component.meta.hasSuggestions;
+                currentComponent.isCurrent = component.isCurrent;
+                currentComponent.usageStartYear = component.usageStartYear;
+                currentCategory[component.meta.componentName] = currentComponent.hasSuggestions ? [(currentComponent)] : (currentComponent);
+            });
+            buildingObject[category.categoryName] = currentCategory;
+        });
+
+
+        console.log(JSON.stringify(buildingObject, null, 4));
+
+        response.status(200).json(buildingObject);
+
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        response.status(500).send("Internal server error");
+    }
 };
 
 module.exports = {
