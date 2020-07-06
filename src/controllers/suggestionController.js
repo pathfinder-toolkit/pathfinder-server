dfcd76f26a449bddf59baf80d1eece132f3ad9fc
Sami Jouppila <sami.jouppila@gmail.com>
2020-07-06 13:24:10 +0300

    Suggestion array is shuffled and value is used to determine amount of suggestions given out

--- src/controllers/suggestionController.js
+++ src/controllers/suggestionController.js
@@ -5,6 +5,7 @@ const sequelize = db.sequelize;
 const {
     makeExampleSuggestions
 } = require('./suggestionUtils/suggestionCreation');
+const { Suggestion, ComponentMeta } = require('../models');
 
 const findSuggestionsFromParams = async (request, response) => {
     const t = await sequelize.transaction();
@@ -14,6 +15,36 @@ const findSuggestionsFromParams = async (request, response) => {
         //await makeExampleSuggestions( t );
 
         
+
+        const suggestions = await Suggestion.findAll({
+            attributes: ['suggestionText', 'suggestionSecondarySubject', 'priority', 'idSuggestion'],
+            include: {
+                model: ComponentMeta,
+                as : 'subject',
+                where: {
+                    componentName : subject
+                },
+                attributes:['subject']
+            }
+        });
+
+        const shuffleArray = (array) => {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        };
+
+        shuffleArray(suggestions);
+
+        let amount = Math.min( Math.abs(value), suggestions.length );
+        
+        const selectedSuggestions = suggestions.slice(0, amount);
+
+        for (const suggestion of selectedSuggestions) {
+            console.log(suggestion.toJSON());
+        }
+
         console.log("rollback for safety");
         await t.rollback();
         response.status(200).send("Success: " + subject + " " + value);
