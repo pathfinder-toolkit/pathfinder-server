cd3d7309196cd65b75315b1443cb201ec33c402a
Sami Jouppila <sami.jouppila@gmail.com>
2020-07-09 11:09:57 +0300

    Image is received in backend, cloudinary upload in progress

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -5,28 +5,38 @@ const cloudinary = require('../utils/cloudinary');
 
 const sequelize = db.sequelize;
 
+
+
 const uploadImageToCloudinary = async (request, response) => {
     const t = await sequelize.transaction();
     try {
-        //const file = 'pizza.jpg';
-        const author = 'usersub';
 
-        const cloudinaryResponse = await cloudinary.uploader.upload(file, {upload_preset: 'pathfinder_userimages'});
-        console.log(cloudinaryResponse);
+        if (request.files.image.truncated === false) {
+            const author = request.user.sub;
+            console.log(author);
+            const image = request.files.image;
+            console.log(image);
+            console.log(image.data);
+            /*const cloudinaryResponse = await cloudinary.uploader.upload(image.data, {upload_preset: 'pathfinder_userimages', resource_type: "raw"});
+            console.log(cloudinaryResponse);
 
-        const publicId = cloudinaryResponse.public_id;
+            const publicId = cloudinaryResponse.public_id;
 
-        const image = await Image.create({
-            image: publicId,
-            authorSub: author
-        },
-        {transaction: t});
+            const image = await Image.create({
+                image: publicId,
+                authorSub: author
+            },
+            {transaction: t});
 
-        console.log(image.toJSON());
+            console.log(image.toJSON());*/
 
-        console.log("rollback for safety");
-        await t.rollback();
-        response.status(200).send("Success!")
+            console.log("rollback for safety");
+            await t.rollback();
+            response.status(200).send("Success!")
+        } else {
+            await t.rollback();
+            response.status(413).send("File size limit has been reached, upload unsuccessful");
+        }
     } catch (error) {
         await t.rollback();
         console.log(error);
--- src/routes/image.js
+++ src/routes/image.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const imageRouter = express.Router();
 
+const fileUpload = require("express-fileupload");
+
 const {
     checkJwt
   } = require('../utils/auth');
 
 const { uploadImageToCloudinary } = require('../controllers/imageController');
+
+imageRouter.use(fileUpload({
+    limits: { 
+        fileSize: 10 * 1024 * 1024
+    }
+}));
 
 imageRouter.post('/image', checkJwt, uploadImageToCloudinary);
 
