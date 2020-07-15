2c1693687d1f5edaa72f58d42cb8e27465aca1f1
Sami Jouppila <sami.jouppila@gmail.com>
2020-07-15 11:37:49 +0300

    recaptcha verification works in backend

--- src/controllers/feedbackController.js
+++ src/controllers/feedbackController.js
@@ -1,6 +1,30 @@
+const axios = require('axios');
+
 const checkRecaptcha = async (request, response, next) => {
-    console.log(request.body);
-    next();
+    try {
+        console.log(request.body);
+        const recaptcha = request.body.recaptcha;
+
+        const address = encodeURI(
+            "https://www.google.com/recaptcha/api/siteverify?"
+            + "secret=" + process.env.GOOGLE_RECAPTCHA_SECRET
+            + "&response=" + recaptcha
+        );
+
+        const response = await axios.post(address);
+        console.log(response.data);
+        
+        if (response.data.success) {
+            console.log("recaptcha verified");
+            next();
+        } else {
+            throw new Error("Your feedback failed to process due to incorrect recaptcha.");
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(400).send(error.message);
+    }
+    
 }
 
 const sendFeedback = async (request, response) => {
